da41bb1fd3dd85b042c65d1df242a9b39c8585ba
Paul Schuetz <pa.schuetz@web.de>
2023-05-27 09:37:22 +0200

    Initial Commit

---
+++ Quizzer/QuizzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizzerApp.swift
+//  Quizzer
+//
+//  Created by Paul on 27.05.23.
+//
+
+import SwiftUI
+
+@main
+struct QuizzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
