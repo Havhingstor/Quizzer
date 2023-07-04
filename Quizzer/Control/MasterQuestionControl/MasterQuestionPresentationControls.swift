b0320713b6fe74082315ef10ff5987944a22b4a1
Paul Schuetz <pa.schuetz@web.de>
2023-07-04 14:08:01 +0200

    Show the right char for master question options
    The letter for the next shown master question option is now correct.
    
    Signed-off-by: Paul Schuetz <pa.schuetz@web.de>

--- Quizzer/Control/MasterQuestionControl/MasterQuestionPresentationControls.swift
+++ Quizzer/Control/MasterQuestionControl/MasterQuestionPresentationControls.swift
@@ -70,7 +70,7 @@ struct MasterQuestionPresentationControls: View {
     }
     
     func getChar(stage: Int) -> String {
-        let firstCharIndex = maxIndexPreOption + 1
+        let firstCharIndex = maxIndexPreOption
         let index = UInt(abs(stage - firstCharIndex))
         return MasterQuestion.getAlphabeticalNr(for: index)
     }
